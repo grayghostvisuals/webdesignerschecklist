a56b513b61f8594a474aadd2cc1cae71c7056fff
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-08-12 19:03:42 -0400

    Built VDCW from commit fdb220b on branch master

--- scripts/c20efb89.main.js
+++
@@ -1,1 +0,0 @@
-function scrollLink(a){"use strict";$("html, body").animate({scrollTop:$($.attr(this,"href")).offset().top},300),a.preventDefault()}$(".scroll-arrow").on("click","a",scrollLink),$(".scroll-arrow").on("touchstart","a",scrollLink),$("form").garlic();
